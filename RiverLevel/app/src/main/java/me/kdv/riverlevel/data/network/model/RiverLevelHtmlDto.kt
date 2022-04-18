0f7f199af35ab66de959771a44de9e42e6d86402
mephres <45750684+mephres@users.noreply.github.com>
2022-04-18 17:26:40 +0500

    Delete RiverLevel directory

--- RiverLevel/app/src/main/java/me/kdv/riverlevel/data/network/model/RiverLevelHtmlDto.kt
+++
@@ -1,9 +0,0 @@
-package me.kdv.riverlevel.data.network.model
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-data class RiverLevelHtmlDto(
-    @Expose
-    val riverLevelHtml: String
-)
